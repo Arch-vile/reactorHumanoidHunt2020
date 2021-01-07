4db4310c998cc98effdd47378c302a5e59761a45
Arch-vile <mikko.ravimo@gmail.com>
2021-01-07 21:53:07 +0200

    Part 1 complete

--- src/main/kotlin/Part1.kt
+++ src/main/kotlin/Part1.kt
@@ -1,4 +1,21 @@
+import java.io.File
 
 fun main(args: Array<String>) {
+  val values = File("./src/main/resources/part1Input.txt")
+    .readLines()
+    .map { it.windowed(8,8).map { it.toInt(2) } }
 
+  values
+    .map { findPassLetter(it) }
+    .map { it.toChar() }
+    .joinToString("")
+    .let { println(it) }
+}
+
+fun findPassLetter(row: List<Int>): Int {
+  var index = row.dropWhile { it >= row.size }.first()
+  while (index < row.size) {
+   index = row[index]
+  }
+  return index
 }
